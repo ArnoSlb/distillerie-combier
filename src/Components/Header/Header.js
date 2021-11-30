15d13b98e2960770149031bb05dc96381962e8c4
Arnaud Blinkl <arnaud@blinkl.com>
2021-11-30 11:00:38 +0100

    feat(lang state): add lang state and how to change it between fr and eng

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,74 @@
-import React,{ useState } from "react";
+import React,{ useState, useRef, useEffect } from "react";
 
 import "./Header.css";
 
 import CombierLogo from "../../assets/Combier_Distil_W.png"
 import FranceFlag from "../../assets/france-flag-round-small.png"
+import EnglishFlag from "../../assets/english-flag-round-small.png"
 
 
 const Header = () => {
+    
 
-    let [lang, setLang] = useState('FR')
+    const [lang, setLang] = useState('FR')
+    const [langToggle, setLangToggle] = useState(false)
+
+    const ChangeLangToggle = () => {
+        setLangToggle(!langToggle)
+    }
 
     const eshopRedirection = () => {
         window.open('https://www.combier.fr/signatures/tous-les-produits-signatures/', "_blank") || window.location.replace('https://www.combier.fr/signatures/tous-les-produits-signatures/');
     }
 
+    const showLang =() => {
+        if(langToggle == false ){
+            if(lang == 'FR'){
+                return (
+                    <div className="Header__container__lang__FR">
+                        <img className="Header__container__lang__img" src={FranceFlag} alt="" />
+                        <p>FR</p>
+                    </div>
+                )
+            } else if (lang == 'EN'){
+                return (
+                    <div className="Header__container__lang__EN">
+                        <img className="Header__container__lang__img" src={EnglishFlag} alt="" />
+                        <p>EN</p>
+                    </div>
+                )
+            }
+
+        } else if (langToggle == true){
+            return (
+            <>
+                <div className="Header__container__lang__FR" onClick={selectFR}>
+                    <img className="Header__container__lang__img" src={FranceFlag} alt="" />
+                    <p>FR</p>
+                </div>
+                <div className="Header__container__lang__EN" onClick={selectEN}>
+                    <img className="Header__container__lang__img" src={EnglishFlag} alt="" />
+                    <p>EN</p>
+                </div>
+            </>
+            )
+        }
+    }
+
+    const selectFR = () => {
+        setLang('FR')
+    }
+
+    const selectEN = () => {
+        setLang('EN')
+    }
 
     return (
         <div className="Header">
             <img className="Header__img" src={CombierLogo} alt="" />
             <div className="Header__container">
-                <div className="Header__container__lang">
-                    <img className="Header__container__lang__img" src={FranceFlag} alt="" />
-                    <p>FR</p>
+                <div className="Header__container__lang" onClick={ChangeLangToggle}>
+                    {showLang()}
                 </div>
                 <div className="Header__container__eshop" onClick={eshopRedirection}>Acheter</div>
             </div>
