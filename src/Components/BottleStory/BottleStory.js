c28fa372b89ae568aaad9e31a4e155197f2fb7e6
Arnaud Blinkl <arnaud@blinkl.com>
2021-12-06 15:16:52 +0100

    fix(scroll issue when bottle changed): fix issue when an other bottle than elixir was selected. I removed the event listener when the component is unmounted

--- src/Components/BottleStory/BottleStory.js
+++ src/Components/BottleStory/BottleStory.js
@@ -10,12 +10,9 @@ const BottleStory = (props) => {
         return (<OriginalCombier/>)
     } else if(props.bottle == "elixir"){
         return (<ElixirCombier/>)
-    }
-
-    console.log(props.bottle)
-    return (
-        <h1>{props.bottle}</h1>
-    )
+    } else {
+        return (<h1>{props.bottle}</h1>)
+    }  
 }
 
 export default BottleStory
--- src/Components/ElixirCombier/ElixirCombier.js
+++ src/Components/ElixirCombier/ElixirCombier.js
@@ -1,34 +1,45 @@
 import React from 'react'
 
 import "./ElixirCombier.css"
+
+import ElixirHD from "../../assets/Elixir_HD.jpg"
  
 const ElixirCombier = () => {
 
     React.useEffect(() => {
-        
+
         // si on est rendu en bas de la page alors .ElixierCombier.style.overflowX: hidden; */
         // else overlowX: initial
         //Sinon le scroll passe en horizontal alors qu'on est pas rendu en bas de la page
 
-        window.addEventListener('scroll', () => {
+        const scrollToRight = () => {
+
             const {scrollTop, scrollHeight, clientHeight} = document.documentElement
 
             console.log(scrollTop, clientHeight, scrollHeight)
 
-            if( scrollTop + clientHeight == scrollHeight){
-                console.log("je passe en scroll horizontal")
-                document.querySelector('.ElixirCombier').style.overflowX = "hidden";
-            } else {
-                document.querySelector('.ElixirCombier').style.overflowX = "initial";
-            }
-        })
-    },[])
+                if( scrollTop + clientHeight == scrollHeight){
+                    console.log("je passe en scroll horizontal")
+                    document.querySelector('.ElixirCombier').style.overflowX = "hidden";
+                } else {
+                    document.querySelector('.ElixirCombier').style.overflowX = "initial";
+                }
+        }
 
+        window.addEventListener('scroll', scrollToRight)
+        
+        return () => {
+            window.removeEventListener('scroll', scrollToRight)
+            }
+    },[]);
 
     return (
         <div className="ElixirCombier">
             <div className="ElixirCombier__HorizontalBlock">
-                <div className="ElixirCombier__HorizontalBlock__slide one">Slide 1</div>
+                <div className="ElixirCombier__HorizontalBlock__slide one">
+                    <p>Slide 1</p>
+                    <img src={ElixirHD} alt="" />
+                </div>
                 <div className="ElixirCombier__HorizontalBlock__slide two">Slide 2</div>
                 <div className="ElixirCombier__HorizontalBlock__slide three">Slide 3</div>
                 <div className="ElixirCombier__HorizontalBlock__slide four">Slide 4</div>
