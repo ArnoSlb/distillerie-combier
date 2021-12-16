12409a54858362e4e972823856b5a9d838853a23
Arnaud Blinkl <arnaud@blinkl.com>
2021-12-16 09:45:04 +0100

    feat(absinthe): create absinthe blanchette component

---
+++ src/Components/AbsintheBlanchette/AbsintheBlanchette.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./AbsintheBlanchette.css"
+
+const AbsintheBlanchette = () => {
+    return (
+        <div className="AbsintheBlanchette"></div>
+    )
+}
+
+export default AbsintheBlanchette
