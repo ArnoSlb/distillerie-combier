c7b4ae968c5fc0a0bbcd5253eac0709b3f5591bc
Arnaud Blinkl <arnaud@blinkl.com>
2022-06-07 17:52:22 +0200

    feat(masonry-css): install masonry-css

--- src/Components/ElixirCombier/ElixirSectionWipesDesktop/ElixirGallery/ElixirGallery.js
+++ src/Components/ElixirCombier/ElixirSectionWipesDesktop/ElixirGallery/ElixirGallery.js
@@ -1,9 +1,39 @@
 import react from "react";
+import Masonry from "react-masonry-css";
+
+import "./ElixirGallery.css"
 
 const ElixirGallery = () => {
+
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 3,
+        700: 2,
+        500: 1
+      };
+
+    var items = [
+    {id: 1, name: 'My First Item'},
+    {id: 2, name: 'Another item'},
+    {id: 3, name: 'Third Item'},
+    {id: 4, name: 'Here is the Fourth'},
+    {id: 5, name: 'High Five'}
+    ];
+    
+    // Convert array to JSX items
+    items = items.map(function(item) {
+        return <div key={item.id}>{item.name}</div>
+    });
+
     return(
         <div id="ElixirGallery">
-            <h1>Test Elixir Gallery</h1>
+           <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+            >
+            {items}
+            </Masonry>
         </div>
     )
 }
